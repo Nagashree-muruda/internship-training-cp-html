8d9adb135a4b86979c524191957ff65412f1ae00
whitespaces-cloud <whitespaces@wealthbeacon.ai>
2026-03-17 10:23:14 +0530

    Added Arrays in components

--- javascript/ReactJS/my-first-react-app/src/App.js
+++ javascript/ReactJS/my-first-react-app/src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import Test from './Component/Test';
 import User from './Component/User';
 import Greetings from './Component/Greetings';
+import UserList from './Component/UserList';
 function App() {
+
+  const users = [
+    { id:1, name: 'Jacob Dennis', age: 25 },
+    { id:2, name: 'Jane Doe', age: 30 },
+    { id:3, name: 'John Smith', age: 28 }
+  ]
+
   return (
     <>
-      <User/>
-      <Greetings name="Jacob Dennis" age={25} />
+      <UserList users={users} />
+      
     </>
   );
 }
